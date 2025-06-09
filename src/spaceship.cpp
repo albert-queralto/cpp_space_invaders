a18cb07630f5efcd239919e825f2ab111eefec88
albert-queralto <albert.queralto.lopez@gmail.com>
2025-06-09 17:33:51 +0200

    Add Spaceship class with movement, drawing, and laser firing functionality

---
+++ src/spaceship.cpp
@@ -0,0 +1,45 @@
+#include "spaceship.hpp"
+#include <raylib.h>
+
+Spaceship::Spaceship()
+{
+    image = LoadTexture("assets/images/spaceship.png");
+    position.x = (GetScreenWidth() - image.width) / 2.0f;
+    position.y = GetScreenHeight() - image.height - 10;
+    lastFireTime = 0.0;
+}
+
+Spaceship::~Spaceship()
+{
+    UnloadTexture(image);
+}
+
+void Spaceship::Draw()
+{
+    DrawTextureV(image, position, WHITE);
+}
+
+void Spaceship::MoveLeft()
+{
+    position.x -= 7.0f;
+    if (position.x < 0) {
+        position.x = 0; // Prevent moving out of bounds
+    }
+}
+
+void Spaceship::MoveRight()
+{
+    position.x += 7.0f;
+    if (position.x > GetScreenWidth() - image.width) {
+        position.x = GetScreenWidth() - image.width; // Prevent moving out of bounds
+    }
+}
+
+void Spaceship::FireLaser()
+{
+    if (GetTime() - lastFireTime >= 0.35) {
+        lasers.push_back(Laser(Vector2{position.x + image.width / 2.0f - 2.5f, position.y}, -6.0f));
+        lastFireTime = GetTime();
+    }
+    
+}
