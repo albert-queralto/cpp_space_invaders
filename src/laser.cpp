bc69872f7d3d4ff76ccb2502c0d9f5af69d475d3
albert-queralto <albert.queralto.lopez@gmail.com>
2025-06-09 17:33:34 +0200

    Add Laser class with methods for initialization, updating, and drawing

---
+++ src/laser.cpp
@@ -0,0 +1,26 @@
+#include "laser.hpp"
+#include <iostream>
+
+Laser::Laser(Vector2 position, int speed)
+{
+    this->position = position;
+    this->speed = speed;
+    active = true;
+}
+
+void Laser::Draw() 
+{
+    if (active)
+        DrawRectangle(position.x, position.y, 5, 20, {243, 216, 63, 255});
+}
+
+void Laser::Update() 
+{
+    position.y += speed;
+    if(active) {
+        if (position.y < 0 || position.y > GetScreenHeight()) {
+            active = false; // Deactivate laser if it goes out of bounds
+            // std::cout << "Laser Inactive" << std::endl;
+        }
+    }
+}
