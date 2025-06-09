e9ed2e3e68d6a34a9aebc0ea5a227cc02cc4b2e5
albert-queralto <albert.queralto.lopez@gmail.com>
2025-06-09 17:34:07 +0200

    Refactor build script to include all source files for compilation

--- src/build.bash
+++ src/build.bash
@@ -1,1 +1,9 @@
-g++ main.cpp -lraylib -lGL -lm -lpthread -ldl -lrt -lX11 -o main
+g++ src/main.cpp \
+    src/spaceship.cpp \
+    src/game.cpp \
+    src/laser.cpp \
+    src/obstacle.cpp \
+    src/block.cpp \
+    src/alien.cpp \
+    -lraylib -lGL -lm -lpthread -ldl -lrt -lX11 \
+    -o main
