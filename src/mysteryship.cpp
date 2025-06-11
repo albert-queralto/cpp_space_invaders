f581a15173a4e4e5bde3673f9ebf76e56eafb7a2
albert-queralto <albert.queralto.lopez@gmail.com>
2025-06-11 18:12:09 +0200

    Add MysteryShip class with spawning, updating, and drawing functionality

---
+++ src/mysteryship.cpp
@@ -0,0 +1,43 @@
+#include "mysteryship.hpp"
+
+MysteryShip::MysteryShip()
+{
+    image = LoadTexture("assets/images/mystery.png");
+    alive = false;
+}
+
+MysteryShip::~MysteryShip()
+{
+    UnloadTexture(image);
+}
+
+void MysteryShip::Spawn()
+{
+    position.y = 90;
+    int side = GetRandomValue(0, 1);
+    if (side == 0) {
+        position.x = 0;
+        speed = 3;
+    } else {
+        position.x = GetScreenWidth() - image.width;
+        speed = -3;
+    }   
+    alive = true;
+}
+
+void MysteryShip::Update()
+{
+    if (alive) {
+        position.x += speed;
+        if (position.x < 0 || position.x > GetScreenWidth() - image.width) {
+            alive = false; // Deactivate if it goes out of bounds
+        }
+    }
+}
+
+void MysteryShip::Draw()
+{
+    if (alive) {
+        DrawTextureV(image, position, WHITE);
+    }
+}
